7e319d55c6d67fa7e8a1b850424712cd74e41ef5
Swaroop Chavan <swaroop9503>
2024-07-19 19:26:17 +0530

    Initial commit

---
+++ src/StackExample.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<Integer>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        st.push(60);
+
+//        System.out.println(st.pop());
+//        System.out.println(st.pop());
+//        System.out.println(st.peek());
+//        System.out.println(st.peek());
+//        System.out.println(st.isEmpty());
+
+        while (!st.isEmpty()){
+            System.out.println(st.peek()+" ");
+            st.pop();
+        }
+    }
+}
---
+++ src/StringRevStack.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StringRevStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Stack<Character> st = new Stack<Character>();
+
+        for (int i = 0; i < str.length(); i++) {
+            st.push(str.charAt(i));
+        }
+        String rev= "";
+        while (!st.isEmpty()) {
+            rev = rev + st.pop();
+        }
+
+        System.out.println(rev);
+    }
+}
