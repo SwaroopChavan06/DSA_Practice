1fd3a0c689db3d82e9a5158292aaf7ab606f75d8
Swaroop <swaroopch06@gmail.com>
2024-07-23 18:13:44 +0530

    Stack coding

---
+++ src/StackOdd.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class StackOdd {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= 10; i++) {
+            stack.push(i);
+        }
+
+
+        System.out.println("Original stack: " + stack);
+
+
+        System.out.println("Popping odd integers:");
+        while (!stack.isEmpty()) {
+            int top = stack.peek();
+            if (top % 2 != 0) {
+                System.out.println("Popped: " + stack.pop());
+            } else {
+                stack.pop();
+            }
+        }
+    }
+}
---
+++ src/StackSort.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class StackSort {
+    public static void main(String[] args) {
+        Stack<Integer> stk = new Stack<>();
+        Stack<Integer> temp = new Stack<>();
+
+        stk.push(2);
+        stk.push(3);
+        stk.push(5);
+        stk.push(1);
+        stk.push(6);
+        stk.push(4);
+
+        while (!stk.isEmpty()) {
+            int current = stk.pop();
+            while (!temp.isEmpty() && temp.peek() < current ) {
+                 stk.push(temp.pop());
+            }
+            temp.push(current);
+        }
+        while (!temp.isEmpty()) {
+            System.out.print(temp.pop() + " ");
+        }
+    }
+}
