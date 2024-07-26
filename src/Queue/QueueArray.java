63995c6638c26f9defd0cc8cf6c4851b98f271ed
Swaroop <swaroopch06@gmail.com>
2024-07-26 13:20:28 +0530

    Queue array

--- src/Queue/QueueArray.java
+++ src/Queue/QueueArray.java
@@ -13,6 +13,7 @@ int[] arr = new int[size];
     public int pop() {
         int num=arr[0];
         for (int i = 0; i< size; i++){
+
             arr[i] = arr[i+1];
         }
         return num;
