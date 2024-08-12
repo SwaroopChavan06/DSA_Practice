5a8d36d97ef211c3c8daa7f1cc6e4529a358f033
Swaroop <swaroopch06@gmail.com>
2024-08-12 15:09:14 +0530

    updated tree questions  questions

---
+++ src/TreeDSA/BinarySearchTree/kthLargestElementBST.java
@@ -0,0 +1,8 @@
+package TreeDSA.BinarySearchTree;
+
+public class kthLargestElementBST {
+    public static void main(String[] args) {
+        kthLargestElementBST obj = new kthLargestElementBST();
+
+    }
+}
--- src/TreeDSA/maxTreeElement.java
+++ src/TreeDSA/maxTreeElement.java
@@ -19,9 +19,9 @@ public class maxTreeElement {
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.left.left.right = new Node(8);
-        root.right.left = new Node(6);
-        root.right.right = new Node(7);
+//        root.left.left.right = new Node(8);
+//        root.right.left = new Node(6);
+//        root.right.right = new Node(7);
         maxElement(root);
         System.out.println(max);//max ele in BT
     }
