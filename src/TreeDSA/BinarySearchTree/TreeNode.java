00828ba09b2ff16c1d1af80db164f97e694e6d32
Swaroop <swaroopch06@gmail.com>
2024-08-13 12:16:32 +0530

    BST deletion

---
+++ src/TreeDSA/BinarySearchTree/TreeNode.java
@@ -0,0 +1,12 @@
+package TreeDSA.BinarySearchTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
