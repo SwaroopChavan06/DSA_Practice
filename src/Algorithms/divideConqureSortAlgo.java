f047896e2686c83ccae72bb6707b8edbdb8c066a
Swaroop <swaroopch06@gmail.com>
2024-08-26 12:19:56 +0530

    JKBLBLBH[ILIN

--- src/Algorithms/divideConqureSortAlgo.java
+++ src/Algorithms/divideConqureSortAlgo.java
@@ -5,7 +5,7 @@ public class divideConqureSortAlgo {
     public static void merge(int[] arr, int l, int m, int r) {
         int n1 = m - l + 1;
         int n2 = r - m;
-//newe IJNRS;  OKJPSEJPOJGR
+//newe HVKVKV;  OKJPSEJPOJGR
         int[] L = new int[n1];
         int[] R = new int[n2];
 //OIPJGTBDOJTD[HKT
