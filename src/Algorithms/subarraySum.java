95d9b89b12e3bf4f20bb943500598c26b91b4943
Swaroop <swaroopch06@gmail.com>
2024-08-26 21:57:38 +0530

    subarray sum

---
+++ src/Algorithms/subarraySum.java
@@ -0,0 +1,21 @@
+package Algorithms;
+
+public class subarraySum {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int k = 3;
+        int count = 0;
+
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
