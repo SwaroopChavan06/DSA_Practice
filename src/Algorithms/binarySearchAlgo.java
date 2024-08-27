6e648cd598a1a7b0a840a4b08c709eb583b62e92
Swaroop <swaroopch06@gmail.com>
2024-08-27 23:20:03 +0530

    subarray sum

--- src/Algorithms/binarySearchAlgo.java
+++ src/Algorithms/binarySearchAlgo.java
@@ -27,3 +27,4 @@ public class binarySearchAlgo {
         }
     }
 }
+//htfhfghfh
--- src/Algorithms/subarraySum.java
+++ src/Algorithms/subarraySum.java
@@ -1,11 +1,9 @@
 package Algorithms;
-
 public class subarraySum {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
         int k = 3;
         int count = 0;
-
         for (int start = 0; start < nums.length; start++) {
             int sum = 0;
             for (int end = start; end < nums.length; end++) {
@@ -15,7 +13,6 @@ public class subarraySum {
                 }
             }
         }
-
         System.out.println(count);
     }
 }
