c3c1934ddca969bbc65fbd8dc5a4542e79622fd6
Swaroop <swaroopch06@gmail.com>
2024-08-30 15:50:36 +0530

    greedy method for job sequence

--- src/Algorithms/jobSequencingProblem.java
+++ src/Algorithms/jobSequencingProblem.java
@@ -1,38 +1,52 @@
 package Algorithms;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class jobSequencingProblem {
     public static void main(String[] args) {
+//        int[][] jobs={
+//                {0,4,20},
+//                {1,1,10},
+//                {2,1,40},
+//                {3,1,30}
+//        };
+
         int[][] jobs={
-                {0,4,20},
-                {1,1,10},
-                {2,1,40},
-                {3,1,30}
+                {0,2,100},
+                {1,1,19},
+                {2,2,27},
+                {3,1,25},
+                {4,3,15},
         };
 
-//        int[][] jobs={
-//                {0,2,100},
-//                {1,1,19},
-//                {2,2,27},
-//                {3,1,25},
-//                {4,3,15},
-//        };
+        int k = 3;
         int m = jobs.length;
         int res[][]=new int[m][2];
 
         for (int i = 0; i<m; i++){
+            res[i][0]=jobs[i][0];
             res[i][1]=jobs[i][1]*jobs[i][2];
         }
 
         for (int i = 0; i< m; i++){
             for (int j = 0; j < 2; j++){
-                System.out.println(res);
+                System.out.print(res[i][j]+" ");
             }
+            System.out.println();
         }
 
+        Arrays.sort(res, Comparator.comparingInt(row -> row[1]));
+
+        for (int[] i : res) {
+            System.out.println(Arrays.toString(i));
+        }
+        System.out.println();
 
 
-
-
+        for (int i = m-1; i>= k -1; i--){
+            System.out.print(res[i][0]+" ");
+        }
 
     }
 }
