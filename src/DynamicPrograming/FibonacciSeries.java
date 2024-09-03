771373d894dec23134aaf2025ad0476c682f86b8
Swaroop <swaroopch06@gmail.com>
2024-09-03 17:25:35 +0530

    dynamic Programing

---
+++ src/DynamicPrograming/FibonacciSeries.java
@@ -0,0 +1,30 @@
+package DynamicPrograming;
+
+public class FibonacciSeries {
+//    static int fib(int n) {
+//        if (n <= 1) {
+//            return n;
+//        }
+//        return fib(n - 1) + fib(n - 2);
+//    }
+
+    static int[] fibo(int num){
+
+        int arr[] = new int[num+2];
+        arr[0]=0;
+        arr[1]=1;
+        int n = arr.length;
+        for (int i=2; i<n; i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int target = 14;
+        int array[] = fibo(target);
+        for (int i = 0; i < target; i++) {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
