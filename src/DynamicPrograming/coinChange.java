2d94c1ce9d938b72d21ecacc16ed5a0551acddea
Swaroop <swaroopch06@gmail.com>
2024-09-05 21:39:25 +0530

    Coin change Dynamic programing

--- src/DynamicPrograming/coinChange.java
+++ src/DynamicPrograming/coinChange.java
@@ -2,6 +2,9 @@ package DynamicPrograming;
 
 public class coinChange {
     public static void main(String[] args) {
-        for(int i = 0; ;)
+        int coins=15;
+        for(int i = 0; i< coins;i++){
+            System.out.println(i);
+        }
     }
 }
